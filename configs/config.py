67318224a95d877cfa4da1b6f8cdda8a9bf616da
wundari <syal.6cyu@gmail.com>
2026-06-08 13:23:23 +0900

    change train dataset to HFM4_cauldron_tqa

--- configs/config.py
+++ configs/config.py
@@ -137,7 +137,7 @@ class TrainConfig:
     lr_vision_backbone: float = 5e-5  # 0.0005 #
     lr_language_backbone: float = 5e-5  # 0
     val_size: int = 50000
-    batch_size: int = 1  # 2
+    batch_size: int = 2
     gradient_accumulation_steps: int = 8
     max_grad_norm: float = 1.0
     eval_in_epochs: bool = True
@@ -152,9 +152,9 @@ class TrainConfig:
         False  # Indicate if the training should be resumed from a checkpoint of the whole VLM or you want to start from scratch
     )
     # train_dataset_path: str = "HuggingFaceM4/FineVision_concat_shuffled_2"
-    train_dataset_path: str = "HuggingFaceM4/FineVision"
+    train_dataset_path: str = "HuggingFaceM4/the_cauldron"  # "HuggingFaceM4/FineVision"
     train_dataset_name: tuple[str, ...] = (
-        "all",  # "default",
+        "tqa",  # "default",
     )  # ('allava_laion', 'allava_vflan', 'cambrian(filtered)_processed', 'LLaVA_Instruct_150K', 'mmevol', 'sharegpt4o', 'sharegpt4v(coco)', 'sharegpt4v(knowledge)', 'sharegpt4v(llava)', 'sharegpt4v(sam)') # 'vision_flan(filtered)', 'lvis_instruct4v',
 
     # train_dataset_path: str = "HuggingFaceM4/the_cauldron"
