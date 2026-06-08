a03e872355d3ddb1a725fa4ea0ed3e72186af09c
wundari <syal.6cyu@gmail.com>
2026-06-08 13:30:57 +0900

    change dataset

--- configs/config.py
+++ configs/config.py
@@ -152,9 +152,9 @@ class TrainConfig:
         False  # Indicate if the training should be resumed from a checkpoint of the whole VLM or you want to start from scratch
     )
     # train_dataset_path: str = "HuggingFaceM4/FineVision_concat_shuffled_2"
-    train_dataset_path: str = "HuggingFaceM4/the_cauldron"  # "HuggingFaceM4/FineVision"
+    train_dataset_path: str = "HuggingFaceM4/FineVision"
     train_dataset_name: tuple[str, ...] = (
-        "tqa",  # "default",
+        "allava_laion",  # "default",
     )  # ('allava_laion', 'allava_vflan', 'cambrian(filtered)_processed', 'LLaVA_Instruct_150K', 'mmevol', 'sharegpt4o', 'sharegpt4v(coco)', 'sharegpt4v(knowledge)', 'sharegpt4v(llava)', 'sharegpt4v(sam)') # 'vision_flan(filtered)', 'lvis_instruct4v',
 
     # train_dataset_path: str = "HuggingFaceM4/the_cauldron"
